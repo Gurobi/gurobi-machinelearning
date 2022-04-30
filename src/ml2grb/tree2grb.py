3fc8c02a12c1cf30eb81e9df129fee343b9e4326
Pierre Bonami <bonami@gurobi.com>
2022-04-30 10:23:36 +0200

    Make decision tree work with multiple examples

--- src/ml2grb/tree2grb.py
+++ src/ml2grb/tree2grb.py
@@ -34,30 +34,32 @@ class DecisionTree2Grb:
         tree = self.tree
         m = self.model
 
-        nodes = m.addMVar(tree.capacity, vtype=GRB.BINARY)
+        nodes = m.addMVar((X.shape[0], tree.capacity), vtype=GRB.BINARY)
 
         # Intermediate nodes constraints
+        # Can be added all at once
         notleafs = [i for i, j in enumerate(tree.children_left) if j >= 0]
-        m.addConstrs(nodes[i] >= nodes[tree.children_left[i]] for i in notleafs)
-        m.addConstrs(nodes[i] >= nodes[tree.children_right[i]] for i in notleafs)
-        m.addConstrs(nodes[i] <= nodes[tree.children_right[i]] + nodes[tree.children_left[i]] for i in notleafs)
-        m.addConstrs(nodes[tree.children_right[i]] + nodes[tree.children_left[i]] <= 1 for i in notleafs)
+        m.addConstrs(nodes[:, i] >= nodes[:, tree.children_left[i]] for i in notleafs)
+        m.addConstrs(nodes[:, i] >= nodes[:, tree.children_right[i]] for i in notleafs)
+        m.addConstrs(nodes[:, i] <= nodes[:, tree.children_right[i]] +
+                     nodes[:, tree.children_left[i]] for i in notleafs)
+        m.addConstrs(nodes[:, tree.children_right[i]] +
+                     nodes[:, tree.children_left[i]] <= 1 for i in notleafs)
 
         # Node splitting
         for node in range(tree.capacity):
             left = tree.children_left[node]
             right = tree.children_right[node]
             if left < 0:
-                m.addConstr((nodes[node] == 1) >> (y[0, 0] == tree.value[node][0][0]))
+                m.addConstrs((nodes[k, node] == 1) >> (y[k, 0] == tree.value[node][0][0]) for k in range(X.shape[0]))
                 continue
-            m.addConstr((nodes[left] == 1) >> (X[0, tree.feature[node]] <= tree.threshold[node]))
-            m.addConstr((nodes[right] == 1) >> (X[0, tree.feature[node]] >= tree.threshold[node] + 0.01))
-            # m.addConstr(x[0,tree.feature[node]] <= tree.threshold[node] + 2 * (1 - nodes[left]))
-            # m.addConstr(x[0,tree.feature[node]] >= tree.threshold[node] + 0.01 - 2 * (1 - nodes[right]))
+            m.addConstrs((nodes[k, left] == 1) >> (X[k, tree.feature[node]] <= tree.threshold[node]) for k in range(X.shape[0]))
+            m.addConstrs((nodes[k, right] == 1) >> (X[k, tree.feature[node]] >= tree.threshold[node] + 0.01) for k in range(X.shape[0]))
 
         # We should attain 1 leaf
-        m.addConstr(quicksum([nodes[i] for i in range(tree.capacity)
-                              if tree.children_left[i] < 0]) == 1)
+        m.addConstrs(quicksum([nodes[k, i] for i in range(tree.capacity)
+                              if tree.children_left[i] < 0]) == 1
+                     for k in range(X.shape[0]))
 
         y.LB = np.min(tree.value)
         y.UB = np.max(tree.value)
