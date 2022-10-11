71dfdc1e964f806b57af6acd4255bd17450ec76c
Pierre Bonami <bonami@gurobi.com>
2022-10-11 18:55:10 +0200

    Add more test for mistakes in input/output variables

--- tests/test_remove.py
+++ tests/test_remove.py
@@ -3,9 +3,10 @@ import random
 import unittest
 
 import gurobipy as gp
-from base_cases import DiabetesCases
+from base_cases import DiabetesCases, IrisCases
 
 from gurobi_ml import add_predictor_constr
+from gurobi_ml.exceptions import ModelingError
 
 
 class TestAddRemove(unittest.TestCase):
@@ -53,6 +54,38 @@ class TestAddRemove(unittest.TestCase):
             self.assertEqual(model.NumGenConstrs, 0)
             self.assertEqual(model.NumQConstrs, 0)
             self.assertEqual(model.NumVars, numvars)
+
+    def add_remove_wrong_input(self, predictor, input_shape, output_shape, nonconvex):
+        """Add and remove the predictor to model"""
+        input_shape, output_shape = self.truncate_shapes(input_shape, output_shape)
+        a, b = input_shape
+        assert b > 1
+        with gp.Model() as model:
+            # Create a variable with wrong shape
+            input_shape = a + 1, b + 1
+            x = model.addMVar(input_shape, lb=-gp.GRB.INFINITY)
+            y = model.addMVar(output_shape, lb=-gp.GRB.INFINITY)
+            model.update()
+            numvars = model.numvars
+
+            model.Params.OutputFlag = 0
+            with self.assertRaises(ModelingError):
+                # All of these should fail
+
+                # Both dimensions too big
+                pred2grb = add_predictor_constr(model, predictor, x, y)
+
+                # Second dimension (features) too big
+                pred2grb = add_predictor_constr(model, predictor, x.reshape(a, b + 1), y)
+
+                # Second dimension (features too small
+                pred2grb = add_predictor_constr(model, predictor, x.reshape(a, b - 1), y)
+
+                # Empty input variable
+                pred2grb = add_predictor_constr(model, predictor, None, y)
+
+                # Mismatch in input output dimensions
+                pred2grb = add_predictor_constr(model, predictor, x.reshape(a - 1, b), y)
 
     def add_remove_no_output(self, predictor, input_shape, output_shape, nonconvex):
         """Add and remove the predictor to model"""
@@ -142,3 +175,62 @@ class TestAddRemove(unittest.TestCase):
         onecase = cases.get_case(regressor)
         with self.subTest(predictor=onecase["predictor"]):
             self.add_remove_list_input(**onecase)
+
+    def test_diabetes_wrong_input(self):
+        """Test adding and removing a predictor for diabetes
+
+        Checks that variables/constraints/... counts match.
+        """
+        cases = DiabetesCases()
+        regressor = random.choice(list(cases))
+
+        onecase = cases.get_case(regressor)
+        with self.subTest(predictor=onecase["predictor"]):
+            self.add_remove_wrong_input(**onecase)
+
+    def test_iris_with_outputvar(self):
+        """Test adding and removing a predictor for iris
+
+        Checks that variables/constraints/... counts match.
+        """
+        cases = IrisCases()
+
+        for regressor in cases:
+            onecase = cases.get_case(regressor)
+            with self.subTest(predictor=onecase["predictor"]):
+                self.add_remove(**onecase)
+
+    def test_iris_without_outputvar(self):
+        """Test adding and removing a predictor for iris
+
+        Checks that variables/constraints/... counts match.
+        """
+        cases = IrisCases()
+        for regressor in cases:
+            onecase = cases.get_case(regressor)
+            with self.subTest(predictor=onecase["predictor"]):
+                self.add_remove_no_output(**onecase)
+
+    def test_iris_varlist(self):
+        """Test adding and removing a predictor for iris
+
+        Checks that variables/constraints/... counts match.
+        """
+        cases = IrisCases()
+        regressor = random.choice(list(cases))
+
+        onecase = cases.get_case(regressor)
+        with self.subTest(predictor=onecase["predictor"]):
+            self.add_remove_list_input(**onecase)
+
+    def test_iris_wrong_input(self):
+        """Test adding and removing a predictor for iris
+
+        Checks that variables/constraints/... counts match.
+        """
+        cases = IrisCases()
+        regressor = random.choice(list(cases))
+
+        onecase = cases.get_case(regressor)
+        with self.subTest(predictor=onecase["predictor"]):
+            self.add_remove_wrong_input(**onecase)
