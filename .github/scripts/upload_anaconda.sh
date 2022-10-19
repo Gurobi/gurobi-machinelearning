2e99ed60de176a86a10958d1f327077a2802d918
Thomas Braam <braam@gurobi.com>
2022-10-19 15:28:49 +0200

    add upload to staging

---
+++ .github/scripts/upload_anaconda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+
+export PATH=$CONDA/bin:$PATH
+conda create -n upload -y python=3.10
+source activate upload
+conda install -y anaconda-client
+
+anaconda -t ANACONDA_STAGING_TOKEN upload --force -u gurobi-machinelearning-wheels-staging dist/artifact/*
