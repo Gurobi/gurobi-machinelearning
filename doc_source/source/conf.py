76f1309aca5488edaf5bc4af9552cd8c5b1c741e
Thomas Braam <braam@gurobi.com>
2022-10-21 16:51:26 +0200

    Revert "Restructure TOC"
    This reverts commit 06e3f9c0aea538ca11863086fd5dace8cc7b456b.

--- doc_source/source/conf.py
+++ doc_source/source/conf.py
@@ -19,7 +19,6 @@ sys.path.insert(0, os.path.abspath("../../src/"))
 # -- Project information -----------------------------------------------------
 config = SphinxConfig("../../pyproject.toml", globalns=globals())
 
-project = "Gurobi Machine Learning"
 copyright = "2022, Gurobi Optimization, LLC. All Rights Reserved."
 html_logo = "_static/image8.png"
 
---
+++ doc_source/source/examples.rst
@@ -0,0 +1,9 @@
+Examples
+========
+
+.. toctree::
+   :maxdepth: 1
+
+   examples/adversarial_mnist.md
+   examples/student_admission.md
+   examples/price_optimization.md
