7b40478d03d63a68663d6b1f9fe06224792a17e0
Thomas Braam <braam@gurobi.com>
2022-10-25 09:31:30 +0200

    [#45] Better output

--- .github/hooks/license_embedded.py
+++ .github/hooks/license_embedded.py
@@ -5,15 +5,14 @@ from pathlib import Path
 expected_license = Path("copyright.txt").read_text().rstrip()
 
 
-def python_check(content: str, cur_file: str) -> int:
+def python_check(content: str, cur_file: str) -> str:
     if not content.startswith(expected_license):
-        print(f"'{cur_file}' did not start with copyright.txt content")
-        return 1
+        return f"'{cur_file}' did not start with copyright.txt content"
     else:
-        return 0
+        return ""
 
 
-def notebook_check(content: str, cur_file: str) -> int:
+def notebook_check(content: str, cur_file: str) -> str:
     j = json.loads(content)
     code = 0
     try:
@@ -23,25 +22,30 @@ def notebook_check(content: str, cur_file: str) -> int:
         code = 1
 
     if code == 1:
-        print(
+        return (
             f"'{cur_file}' does not have the copyright.txt content as metadata. "
             f"This should go into metadata.license.full_text"
         )
-    return code
+    else:
+        return ""
 
 
-def check_file(cur_file: str) -> int:
+def check_file(cur_file: str) -> str:
     file_content = Path(cur_file).read_text()
     if cur_file.endswith(".py"):
         return python_check(file_content, cur_file)
     elif cur_file.endswith(".ipynb"):
         return notebook_check(file_content, cur_file)
     else:
-        print(f"Incorrect file passed: '{cur_file}', expecting '.py' or '.ipynb'")
-        return 1
+        return f"Incorrect file passed: '{cur_file}', expecting '.py' or '.ipynb'"
 
 
 exit_code = 0
 for cur in sys.argv[1:]:
-    exit_code = max(exit_code, check_file(cur))
+    if error := check_file(cur):
+        print(f"License check FAILED for: '{cur}': {error}")
+        exit_code = 1
+    else:
+        print(f"License check passed for: '{cur}'")
+
 exit(exit_code)
