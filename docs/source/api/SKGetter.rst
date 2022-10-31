7c4ae86a5ead6bc2d2534752829a9f8ae5749523
Pierre Bonami <bonami@gurobi.com>
2022-10-31 16:54:18 +0100

    Missing files

---
+++ docs/source/api/SKGetter.rst
@@ -0,0 +1,5 @@
+Scikit-Learn Helper
+===================
+
+.. autoclass:: gurobi_ml.sklearn.skgetter.SKgetter
+    :members:
---
+++ src/gurobi_ml/sklearn/base_regressions.py
@@ -0,0 +1,62 @@
+# Copyright © 2022 Gurobi Optimization, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+""" Module for inserting simple Scikit-Learn regression models into a gurobipy model
+
+All linear models should work:
+   - :external+sklearn:py:class:`sklearn.linear_model.LinearRegression`
+   - :external+sklearn:py:class:`sklearn.linear_model.Ridge`
+   - :external+sklearn:py:class:`sklearn.linear_model.Lasso`
+
+"""
+
+
+from ..modeling import AbstractPredictorConstr
+from .skgetter import SKgetter
+
+
+class BaseSKlearnRegressionConstr(SKgetter, AbstractPredictorConstr):
+    """Predict a Gurobi variable using a Linear Regression that
+    takes another Gurobi matrix variable as input.
+    """
+
+    def __init__(
+        self,
+        gp_model,
+        predictor,
+        input_vars,
+        output_vars=None,
+        output_type="",
+        **kwargs,
+    ):
+        self.n_outputs_ = 1
+        SKgetter.__init__(self, predictor, output_type, **kwargs)
+        AbstractPredictorConstr.__init__(
+            self,
+            gp_model,
+            input_vars,
+            output_vars,
+            **kwargs,
+        )
+
+    def add_regression_constr(self):
+        """Add the prediction constraints to Gurobi"""
+        coefs = self.predictor.coef_.reshape(-1, 1)
+        intercept = self.predictor.intercept_
+        self.gp_model.addConstr(self.output == self.input @ coefs + intercept, name="linreg")
+
+    def print_stats(self, file=None):
+        """Print statistics about submodel created"""
+        super().print_stats(file)
