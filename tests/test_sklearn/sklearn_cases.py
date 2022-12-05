cc3a853d4f73b87a2bbbc7054bba0a68f06d460d
Pierre Bonami <bonami@gurobi.com>
2022-12-05 11:21:16 +0100

    For the test cases, generate individual predictors if file is not there
    This is for #100
    
    Also refactor the file that needed it and add a few comments.

--- tests/test_sklearn/sklearn_cases.py
+++ tests/test_sklearn/sklearn_cases.py
@@ -1,4 +1,5 @@
 import os
+from abc import ABC, abstractmethod
 
 import numpy as np
 from joblib import dump, load
@@ -55,12 +56,40 @@ def predictor_as_string(predictor):
     return "_" + type(predictor).__name__.lower()
 
 
-class Cases:
-    """Base class to have cases for testing"""
+class Cases(ABC):
+    """Base class to have cases for testing
 
-    def __init__(self, dataset, excluded=None, regressors=None, transformers=None):
+    This class is used to construct and save the predictors we want to test.
+
+    It is generic in that it doesn't specify the data used.
+
+    Attributes
+    ----------
+    excluded: list
+    List of sklearn predictors that we don't want to test in a specific case
+
+    regressors: list, optional
+    List of sklearn regression models that we want to test for a specific a case.
+    With default value None, test all predictors we know about.
+
+    transformers: list, optional
+    List of sklearn preprocessing objects that we want to test for a specific case.
+    With default value None, test all preprocessing we know about.
+
+    saved_training: int, optional
+    Number of training example we want to save with the predictor.
+    With default value of 0, don't save anything.
+    """
+
+    def __init__(
+        self, dataset, excluded=None, regressors=None, transformers=None, saved_training=0
+    ):
         self.basedir = os.path.join(os.path.dirname(__file__), "..", "predictors")
         self.dataset = dataset
+        self.saved_training = saved_training
+
+        # Filled with get data if needed
+        self._data = None
 
         if regressors is None:
             regressors = [r for r in sklearn_predictors().keys() if r not in excluded]
@@ -86,122 +115,129 @@ class Cases:
             version = None
         if version != sklearn_version:
             print(f"Scikit learn version changed. Regenerate predictors for {dataset}")
-            self.build_predictors()
+            self.build_all_predictors()
             with open(os.path.join(self.basedir, sklearn_version_file), "w") as file_out:
                 print(sklearn_version, file=file_out)
 
     def __iter__(self):
         return self.all_test.__iter__()
 
+    @abstractmethod
+    def load_data(self):
+        """Define this to load data for predictors"""
+        ...
+
+    @property
+    def data(self):
+        if self._data is None:
+            self.load_data()
+        return self._data
+
+    def predictor_file(self, predictor):
+        return f"{self.dataset}_{predictor_as_string(predictor)}.joblib"
+
+    def build_predictor(self, predictor):
+        """Build model for one predictor"""
+        X, y = self.data
+        predictor.fit(X, y)
+        non_convex = False
+        if isinstance(predictor, Pipeline):
+            for element in predictor:
+                if isinstance(element, PolynomialFeatures):
+                    non_convex = True
+                    break
+
+        rval = {
+            "predictor": predictor,
+            "input_shape": X.shape,
+            "output_shape": y.shape,
+            "nonconvex": non_convex,
+        }
+        if self.saved_training:
+            rval["data"] = X[: self.saved_training]
+            rval["target"] = X[: self.saved_training]
+        return rval
+
+    def build_all_predictors(self):
+        """Build all the predictor for this case.
+        (Done when we have a new sklearn version)"""
+        for predictor in self:
+            rval = self.build_predictor(predictor)
+
+            dump(rval, os.path.join(self.basedir, self.predictor_file(predictor)))
+
     def get_case(self, predictor):
-        filename = f"{self.dataset}_{predictor_as_string(predictor)}.joblib"
-        predictor = load(os.path.join(self.basedir, filename))
-        return predictor
+        filename = self.predictor_file(predictor)
+        try:
+            return load(os.path.join(self.basedir, filename))
+        except FileNotFoundError:
+            return self.build_predictor(predictor)
 
 
 class DiabetesCases(Cases):
-    """Base class to have cases for testing regression models on diabetes set"""
+    """Base class to have cases for testing regression models on diabetes set
+
+    This is appropriate for testing a regression with a single output."""
 
     def __init__(self):
         excluded = ["LogisticRegression"]
         super().__init__("diabetes", excluded=excluded)
 
-    def build_predictors(self):
+    def load_data(self):
         data = datasets.load_diabetes()
 
         X = data["data"]
         y = data["target"]
-        for predictor in self:
-            predictor.fit(X, y)
-            filename = f"{self.dataset}_{predictor_as_string(predictor)}.joblib"
-            non_convex = False
-            if isinstance(predictor, Pipeline):
-                for element in predictor:
-                    if isinstance(element, PolynomialFeatures):
-                        non_convex = True
-                        break
-
-            rval = {
-                "predictor": predictor,
-                "input_shape": X.shape,
-                "output_shape": y.shape,
-                "nonconvex": non_convex,
-            }
-
-            dump(rval, os.path.join(self.basedir, filename))
+        self._data = (X, y)
 
 
 class IrisCases(Cases):
-    """Base class to have cases for testing regression models on iris set"""
+    """Base class to have cases for testing regression models on iris set
+
+    Transform the iris test set to binary classification.
+    This is appropriate for testing binary classification models."""
 
     def __init__(self):
         super().__init__("iris", regressors=["LogisticRegression"])
 
-    def build_predictors(self):
+    def load_data(self):
         data = datasets.load_iris()
 
         X = data.data
         y = data.target
-
-        # Make it a simple classification
+        # Make it a binary classification
         X = X[y != 2]
         y = y[y != 2]
-
-        for predictor in self:
-            predictor.fit(X, y)
-            filename = f"{self.dataset}_{predictor_as_string(predictor)}.joblib"
-            non_convex = False
-            if isinstance(predictor, Pipeline):
-                for element in predictor:
-                    if isinstance(element, PolynomialFeatures):
-                        non_convex = True
-                        break
-
-            rval = {
-                "predictor": predictor,
-                "input_shape": X.shape,
-                "output_shape": y.shape,
-                "nonconvex": non_convex,
-            }
-
-            dump(rval, os.path.join(self.basedir, filename))
+        self._data = (X, y)
 
 
 class CircleCase(Cases):
-    def __init__(self):
-        super().__init__("circle", regressors=["DecisionTreeRegressor", "RandomForestRegressor"])
+    """Artificial test case to test multi-output regression models
 
-    def build_predictors(self):
+    Currently we use it for decision trees and random forests."""
+
+    def __init__(self):
+        super().__init__(
+            "circle",
+            regressors=["DecisionTreeRegressor", "RandomForestRegressor"],
+            saved_training=-1,
+        )
+
+    def load_data(self):
         # Inspired bu Scikit-learn example
         # Create a dataset drawing a circle (don't put noise at it's not
         # really relevant here)
         rng = np.random.RandomState(1)
         X = np.sort(200 * rng.rand(100, 1) - 100, axis=0)
         y = np.array([np.pi * np.sin(X).ravel(), np.pi * np.cos(X).ravel()]).T
-
-        for predictor in self:
-            predictor.fit(X, y)
-            filename = f"{self.dataset}_{predictor_as_string(predictor)}.joblib"
-            non_convex = False
-            if isinstance(predictor, Pipeline):
-                for element in predictor:
-                    if isinstance(element, PolynomialFeatures):
-                        non_convex = True
-                        break
-
-            rval = {
-                "predictor": predictor,
-                "input_shape": X.shape,
-                "output_shape": y.shape,
-                "nonconvex": non_convex,
-                "data": X,
-                "target": y,
-            }
-
-            dump(rval, os.path.join(self.basedir, filename))
+        self._data = (X, y)
 
 
 class MNISTCase(Cases):
+    """MNIST test case
+
+    We use it for multi output neural networks regressions."""
+
     def __init__(self):
         super().__init__(
             "mnist",
@@ -209,32 +245,14 @@ class MNISTCase(Cases):
                 "MLPClassifier",
             ],
             transformers=[],
+            saved_training=100,
         )
 
-    def build_predictors(self):
+    def load_data(self):
         mnist = datasets.fetch_openml("mnist_784")
         X, y = mnist.data, mnist.target
 
         X = X.to_numpy()
         y = y.to_numpy()
         X /= 255.0  # scaling
-        for predictor in self:
-            predictor.fit(X, y)
-            filename = f"{self.dataset}_{predictor_as_string(predictor)}.joblib"
-            non_convex = False
-            if isinstance(predictor, Pipeline):
-                for element in predictor:
-                    if isinstance(element, PolynomialFeatures):
-                        non_convex = True
-                        break
-
-            rval = {
-                "predictor": predictor,
-                "input_shape": X.shape,
-                "output_shape": y.shape,
-                "nonconvex": non_convex,
-                "data": X[:100, :],
-                "target": y[:100],
-            }
-
-            dump(rval, os.path.join(self.basedir, filename))
+        self._data = (X, y)
