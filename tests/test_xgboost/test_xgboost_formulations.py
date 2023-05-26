[#177] Try to fix numerical issues in test

tests/test_xgboost/test_xgboost_formulations.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import xgboost as xgb
 from sklearn import datasets
 
@@ -12,7 +13,7 @@ class TestXGBoosthModel(FixedRegressionModel):
 
     basedir = os.path.join(os.path.dirname(__file__), "..", "predictors")
 
-    def test_diabetes_xgboost(self):
+    def test_diabetes_xgboost_pairs(self):
         data = datasets.load_diabetes()
         X = data["data"]
         y = data["target"]
@@ -21,6 +22,15 @@ class TestXGBoosthModel(FixedRegressionModel):
         xgb_reg.fit(X, y)
         one_case = {"predictor": xgb_reg.get_booster(), "nonconvex": 0}
 
-        self.do_one_case(one_case, X, 6, "pairs")
+        self.do_one_case(one_case, X, 6, "pairs", float_type=np.float32)
 
-        self.do_one_case(one_case, X, 5, "all")
+    def test_diabetes_xgboost_all(self):
+        data = datasets.load_diabetes()
+        X = data["data"]
+        y = data["target"]
+
+        xgb_reg = xgb.XGBRegressor(n_estimators=10, max_depth=4, max_leaf_nodes=10)
+        xgb_reg.fit(X, y)
+        one_case = {"predictor": xgb_reg.get_booster(), "nonconvex": 0}
+
+        self.do_one_case(one_case, X, 5, "all", float_type=np.float32, epsilon=1e-3)