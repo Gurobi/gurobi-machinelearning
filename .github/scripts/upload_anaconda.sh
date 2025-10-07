c7b8201cae12c6df1f8c29eb3a4bd9f2aa9c2770
Pierre Bonami <bonami@gurobi.com>
2025-10-07 14:01:11 +0200

    Update actions with new locations of artifacts

--- .github/scripts/upload_anaconda.sh
+++ .github/scripts/upload_anaconda.sh
@@ -5,4 +5,4 @@ set -x
 
 # Install anaconda-client via pip and upload artifacts without requiring conda
 python -m pip install --upgrade anaconda-client
-anaconda -t "$ANACONDA_STAGING_TOKEN" upload --force -u gurobi-machinelearning-wheels-staging dist/artifact/*
+anaconda -t "$ANACONDA_STAGING_TOKEN" upload --force -u gurobi-machinelearning-wheels-staging dist/*
